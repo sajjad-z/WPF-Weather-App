aca818376deb0156c891f21f68eb49321b9566d9
sajjad <assassinsajjad0@gmail.com>
2022-04-06 17:52:01 +0430

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Weather
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void textSearch_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtSearch.Focus();
+        }
+
+        private void txtSearch_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtSearch.Text) && txtSearch.Text.Length > 0)
+                textSearch.Visibility = Visibility.Collapsed;
+            else
+                textSearch.Visibility = Visibility.Visible;
+        }
+
+    }
+}
---
+++ UserControls/CardDay.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Weather.UserControls
+{
+    public partial class CardDay : UserControl
+    {
+        public CardDay()
+        {
+            InitializeComponent();
+        }
+
+
+        public string Day
+        {
+            get { return (string)GetValue(DayProperty); }
+            set { SetValue(DayProperty, value); }
+        }
+
+        public static readonly DependencyProperty DayProperty = DependencyProperty.Register("Day", typeof(string), typeof(CardDay));
+
+
+        public ImageSource Source
+        {
+            get { return (ImageSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register("Source", typeof(ImageSource), typeof(CardDay));
+
+
+        public string MaxNum
+        {
+            get { return (string)GetValue(MaxNumProperty); }
+            set { SetValue(MaxNumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaxNumProperty = DependencyProperty.Register("MaxNum", typeof(string), typeof(CardDay));
+
+
+        public string MinNum
+        {
+            get { return (string)GetValue(MinNumProperty); }
+            set { SetValue(MinNumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MinNumProperty = DependencyProperty.Register("MinNum", typeof(string), typeof(CardDay));
+
+
+    }
+}
